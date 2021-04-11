e1eed72dee8f631c11ad9bf41ba9d46b7188f5ae
William Robersone <william.robersone@gmail.com>
2021-04-10 20:15:24 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>  
+
+      <CarItem />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dfdfdf',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/CarItem/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+import styles from './styles';
+
+const CarItem = (props) => {
+        return (
+            <View style={styles.carContainer}>
+            <ImageBackground 
+            source={require('../../assets/ModelX.jpeg')} 
+            style={styles.image} 
+            />
+
+            <View style={styles.titles}>
+            <Text style={styles.title}>Model S</Text>
+            <Text style={styles.subtitle}>Starting at $56,000</Text>
+            </View>
+        </View>
+        )
+    
+}
+
+export default CarItem;
